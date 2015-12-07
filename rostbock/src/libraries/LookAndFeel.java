5c036ac293f037590060121b69ab19464bfa0de0
Simon Holzmayer <hosi1015@hs-karlsruhe.de>
2015-12-07 23:34:11 +0100

    Warning for LookAndFeel removed

--- rostbock/src/libraries/LookAndFeel.java
+++ rostbock/src/libraries/LookAndFeel.java
@@ -3,8 +3,6 @@ package libraries;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import com.apple.eawt.Application;
-
 public class LookAndFeel {
 	/**
 	 * @author: Simon Holzmayer
@@ -14,9 +12,6 @@ public class LookAndFeel {
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "goShare");
 		// take the menu bar off the jframe
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
-
-		// set the look and feel
-		Application application = Application.getApplication();
 
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
