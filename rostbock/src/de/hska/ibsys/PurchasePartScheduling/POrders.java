972a51e5a47032309d61492c76a5c8b8f53f4974
Simon Holzmayer <hosi1015@hs-karlsruhe.de>
2016-01-10 00:32:39 +0100

    TableCellRenderer

--- rostbock/src/de/hska/ibsys/PurchasePartScheduling/POrders.java
+++ rostbock/src/de/hska/ibsys/PurchasePartScheduling/POrders.java
@@ -9,13 +9,25 @@ import de.hska.ibsys.ProductionPlan.ArticleAmountPair;
 import de.hska.ibsys.help.Definitions;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class POrders extends JPanel {
 
@@ -68,6 +80,13 @@ public class POrders extends JPanel {
 		
 		fillRowData(orders);
 		this.table = new JTable(rowData, Definitions.orderColumnNames);
+
+//		this.table.setModel(new MyTableModel());
+
+//		this.table.
+		
+//		this.table.getColumnModel().getColumn(0).setBackground(Color.MAGENTA);
+		
 		JScrollPane jsp = new JScrollPane(this.table);
 		jsp.setPreferredSize(new Dimension(750, 450));
 		add(jsp);
@@ -111,4 +130,47 @@ public class POrders extends JPanel {
 		this.prognose1 = prognose1;
 	}
 
+}
+class MyTableModel extends DefaultTableModel {
+
+    List<Color> rowColours = Arrays.asList(
+        Color.RED,
+        Color.GREEN,
+        Color.CYAN
+    );
+
+    public void setRowColour(int row, Color c) {
+        rowColours.set(row, c);
+        fireTableRowsUpdated(row, row);
+    }
+
+    public Color getRowColour(int row) {
+        return rowColours.get(row);
+    }
+
+    @Override
+    public int getRowCount() {
+        return 3;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        return String.format("%d %d", row, column);
+    }
+}
+
+class MyTableCellRenderer extends DefaultTableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        MyTableModel model = (MyTableModel) table.getModel();
+        Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        c.setBackground(model.getRowColour(row));
+        return c;
+    }
 }
